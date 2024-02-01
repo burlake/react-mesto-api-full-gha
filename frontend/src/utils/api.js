66b2c7497b2c195ee41acfed68e66d15894e3f4f
Elizaveta Burlakova <es.burlakova@mail.ru>
2024-02-01 23:24:23 +0300

    1st deploy

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -121,7 +121,7 @@ class Api {
 }
 
 const api = new Api({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://api.mesto1.nomoredomainsmonster.ru",
     // headers: {
     //     authorization: "ae20785e-e850-4452-960a-73f188fc9474",
     //     "Content-Type": "application/json",
